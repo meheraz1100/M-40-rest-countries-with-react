bf3f9dd83fdcbfd02757ae0309873af0e6b2e9da
meheraz1100 <meheraz1100@gmail.com>
2024-03-30 19:44:55 +0600

    update Header text

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Countries from './components/Countries/Countries'
 function App() {
   return (
     <>
-      <h1>Rest Countries</h1>
+      <h1>React World Tour</h1>
       <Countries></Countries>
     </>
   )
