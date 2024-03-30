3776047c72974ff654fb4e8c92b2e54556f2928f
meheraz1100 <meheraz1100@gmail.com>
2024-03-30 20:04:03 +0600

    added dynamic background

--- src/components/Countries/Country/Country.jsx
+++ src/components/Countries/Country/Country.jsx
@@ -11,14 +11,14 @@ const Country = ({country}) => {
     }
 
     return (
-        <div className='country'>
+        <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
             <h3>Name: {name?.common}</h3>
             <img src={flags?.png} alt="" />
             <p>Population :{population}</p>
             <p>Area :{area}</p>
             <p><small>Code : {cca3}</small></p>
             <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
-            {visited ? 'I have Visited this country' : 'i want to visit this country'}
+            {visited ? 'I have visited this country' : 'I want to visit this country'}
         </div>
     );
 };
