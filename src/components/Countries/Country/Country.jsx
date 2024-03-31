cc2e4f7052c1846f622b825e64c7bfa3d4e50589
meheraz1100 <meheraz1100@gmail.com>
2024-03-31 16:27:11 +0600

    added mark visited button and add flag button

--- src/components/Countries/Country/Country.jsx
+++ src/components/Countries/Country/Country.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
-    console.log(country)
-    const {name, flags,population, area, cca3} = country;
+
+const Country = ({country, handleVisitedCountry, handleVisitedFlags}) => {
+    // console.log(country)
+    const {name, flags, population, area, cca3} = country;
 
     const [visited, setVisited] = useState(false);
 
@@ -10,15 +11,21 @@ const Country = ({country}) => {
         setVisited(!visited);
     }
 
+    const passWithParams = () => handleVisitedCountry(country);
+
     return (
         <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
-            <h3>Name: {name?.common}</h3>
+            <h3 style={{color : visited ? 'purple' : 'White'}}>{name?.common}</h3>
             <img src={flags?.png} alt="" />
             <p>Population :{population}</p>
             <p>Area :{area}</p>
             <p><small>Code : {cca3}</small></p>
+            <button onClick={() => handleVisitedCountry(country)}>Mark Visited</button>
+            <br />
+            <button onClick={() => handleVisitedFlags(country.flags.png)}>Add Flag</button>
+            <br />
             <button onClick={handleVisited}>{visited ? 'Visited' : 'Going'}</button>
-            {visited ? 'I have visited this country' : 'I want to visit this country'}
+            {visited ? 'I have visited this country' : 'I want to visit'}
         </div>
     );
 };
